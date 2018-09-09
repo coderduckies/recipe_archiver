7f36b2111d0a26deecf08f83f3b1e9efc293dedf
Ruth <hugthebluebow@gmail.com>
2018-09-09 15:17:39 -0700

    added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="keywords" content="cookbook, coderduckies">
+		<meta name="description" content="Try out some of my fav meals!">
+		<meta name="author" content="Ruth Sanchez">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Coder Duckies Cookbook - Home</title>
+		
+	</head>
+	<body>
+		<?php
+			$name = "Ruth";
+			echo "hello, " $name;
+		?>
+	</body>
+</html>
