6ac084a10da5f2bce84938ccfa17ab02ff60d7d4
Ruth <ruth@ucode.com>
2018-09-11 00:11:23 +0000

    Added form page and stylesheet. Form page has basic form.

---
+++ form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+		<meta name="keywords" content="cookbook, coderduckies">
+		<meta name="description" content="Try out some of my fav meals!">
+		<meta name="author" content="Ruth Sanchez">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Recipe Archiver - Form </title>
+    </head>
+    <body>
+        <!--
+            To do:
+            -form
+            -name
+            -ingredients
+            -steps
+            -image
+            -submit
+        -->
+        <form method="post" action="">
+            <input type="text" name="recipe_name"/>
+            <input type="textarea" name="ingredients"/>
+            <input type="textarea" name="steps"/>
+            <span>Upload Image - Coming Soon</span>
+            <input type="submit" value="Submit"/>
+        </form>
+    </body>
+</html>
