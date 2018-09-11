32472e2c5082888872f91a1c2d14353155e8d4d8
Ruth <ruth@ucode.com>
2018-09-11 00:27:37 +0000

    anchor to form

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 		<meta name="author" content="Ruth Sanchez">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Coder Duckies Cookbook - Home</title>
-		
+		<link rel="stylesheet" href="stylesheet.css" type="text/css" />
 	</head>
 	<body>
+		<a href="form.php">Form</a><br>
 		<?php
-			$name = "Ruth";
-			echo "hello, " $name;
+			$name = "World";
+			echo "hello, $name";
 		?>
 	</body>
 </html>
